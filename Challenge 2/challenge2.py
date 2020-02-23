049397c42eb5faaa05ff331750b9e8456581b35e
NickCattley <16906343+NickCattley@users.noreply.github.com>
2020-02-23 17:25:29 +0000

    Started challenge 2.

--- Challenge 2/challenge2.py
+++ Challenge 2/challenge2.py
@@ -1,1 +1,11 @@
 # Challenge Link: http://www.pythonchallenge.com/pc/def/map.html
+
+import string
+
+my_string = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+difference = +2
+new_string = "".join(
+    (chr(97 + (ord(letter) - 97 + difference) % 26) for letter in my_string)
+)
+print(new_string)
